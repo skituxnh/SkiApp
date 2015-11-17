96f922fa8b74eda1b50d8b5b507184ca533fe693
Josh <joshua.a.burt@gmail.com>
2015-11-17 14:13:11 -0700

    Initial Commit

---
+++ SkiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkiApp
+//
+//  Created by JB on 11/17/15.
+//  Copyright © 2015 Josh Burt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
