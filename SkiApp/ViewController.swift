47f13c28950b9b0a0d1e96aefe32c95f78a3f547
Josh <joshua.a.burt@gmail.com>
2015-11-17 14:17:40 -0700

    Testing Remote Repo

--- SkiApp/ViewController.swift
+++ SkiApp/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//Test Remote Repo
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
