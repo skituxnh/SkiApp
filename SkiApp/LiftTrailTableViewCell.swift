7b74ff73badb61b061a4194ee594cb7a34664b28
Josh <joshua.a.burt@gmail.com>
2015-12-07 17:45:13 -0700

    Objects created for lift display

---
+++ SkiApp/LiftTrailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LiftTrailTableViewCell.swift
+//  SkiApp
+//
+//  Created by JB on 12/7/15.
+//  Copyright © 2015 Josh Burt. All rights reserved.
+//
+
+import UIKit
+
+class LiftTrailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
